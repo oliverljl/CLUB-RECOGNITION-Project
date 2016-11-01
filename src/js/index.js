74ded29c068f6066eefad0c81a72344e7268db48
oliverljl <oliverljl@hotmail.com>
2016-11-01 13:03:12 -0400

    refactor version

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,51 @@
-function getTotal(){function t(){var t=0;$(".subInput1").click(function(){$(this).is(":checked")?(t+=parseInt($(this).val()),i+=parseInt($(this).val()),$("#sub1").text(t),$("#total").text(i)):$(this).is(":not(:checked)")&&(t-=parseInt($(this).val()),i-=parseInt($(this).val()),$("#sub1").text(t),$("#total").text(i))})}function s(){var t=0;$(".subInput2").click(function(){$(this).is(":checked")?(t+=parseInt($(this).val()),i+=parseInt($(this).val()),$("#sub2").text(t),$("#total").text(i)):$(this).is(":not(:checked)")&&(t-=parseInt($(this).val()),i-=parseInt($(this).val()),$("#sub2").text(t),$("#total").text(i))})}function e(){var t=0;$(".subInput3").click(function(){$(this).is(":checked")?(t+=parseInt($(this).val()),i+=parseInt($(this).val()),$("#sub3").text(t),$("#total").text(i)):$(this).is(":not(:checked)")&&(t-=parseInt($(this).val()),i-=parseInt($(this).val()),$("#sub3").text(t),$("#total").text(i))})}function a(){var t=0;$(".subInput4").click(function(){$(this).is(":checked")?(t+=parseInt($(this).val()),i+=parseInt($(this).val()),$("#sub4").text(t),$("#total").text(i)):$(this).is(":not(:checked)")&&(t-=parseInt($(this).val()),i-=parseInt($(this).val()),$("#sub4").text(t),$("#total").text(i))})}var i=0;t(),s(),e(),a()}$(document).ready(getTotal());
+ 'use strict';
+ $(document).ready(getTotal());
+
+ function getTotal() {
+     var total = 0;
+     var sub1 = 0;
+     var sub2 = 0;
+     var sub3 = 0;
+     var sub4 = 0;
+     $(document).on('click', '.subInput1,.subInput2,.subInput3,.subInput4', function() {
+         if ($(this).is(":checked")) {
+             if ($(this)[0].className == "subInput1") {
+                 sub1 += parseInt(($(this).val()));
+                 $('#sub1').text(sub1);
+             }
+             if ($(this)[0].className == "subInput2") {
+                 sub2 += parseInt(($(this).val()));
+                 $('#sub2').text(sub2);
+             }
+             if ($(this)[0].className == "subInput3") {
+                 sub3 += parseInt(($(this).val()));
+                 $('#sub3').text(sub3);
+             }
+             if ($(this)[0].className == "subInput4") {
+                 sub4 += parseInt(($(this).val()));
+                 $('#sub4').text(sub4);
+             }
+             total += parseInt(($(this).val()));
+             $('#total').text(total);
+         } else if ($(this).is(":not(:checked)")) {
+             total -= parseInt(($(this).val()));
+             $('#total').text(total);
+             if ($(this)[0].className == "subInput1") {
+                 sub1 -= parseInt(($(this).val()));
+                 $('#sub1').text(sub1);
+             }
+             if ($(this)[0].className == "subInput2") {
+                 sub2 -= parseInt(($(this).val()));
+                 $('#sub2').text(sub2);
+             }
+             if ($(this)[0].className == "subInput3") {
+                 sub3 -= parseInt(($(this).val()));
+                 $('#sub3').text(sub3);
+             }
+             if ($(this)[0].className == "subInput4") {
+                 sub4 -= parseInt(($(this).val()));
+                 $('#sub4').text(sub4);
+             }
+         }
+     });
+ }
